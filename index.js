aba86d25c6de5913bad171b11e51a48eb4107984
Chris <chriswndegwa@gmail.com>
2024-10-21 11:50:21 +0300

    fetch and DOM render

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books') // Return the promise from fetch
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          renderBooks(data);
+          return data; // Optionally return the data for further use
+      })
+      .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+      });
   
 }
 
